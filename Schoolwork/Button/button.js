aeef4325a8730dfbe8ce9317fb968a6597e5c096
Leo Staf <lest0823@storsjogymnasiet.se>
2023-09-04 14:18:45 +0200

    eight commit

---
+++ Schoolwork/Button/button.js
@@ -0,0 +1,31 @@
+let amount = 0;
+
+const decbutton = document.getElementById("decbutton");
+
+function add() {
+    amount++;
+    if (amount < 10) {
+        document.getElementById("amount").innerHTML = "0" + amount;
+    }
+    else {
+        document.getElementById("amount").innerHTML = amount;
+    }
+}
+
+decbutton.addEventListener("click", decrease);
+
+function decrease() {
+    if (amount > 0) {
+        amount--;
+    }
+    
+    if (amount < 10) {
+        document.getElementById("amount").innerHTML = "0" + amount;
+    }
+    else if (amount) {
+        document.getElementById("amount").innerHTML = amount;
+    }
+}
+
+
+
