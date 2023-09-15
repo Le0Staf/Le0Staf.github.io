1dc1cc7807514fefb55c3afeff9e87b273d99627
Leo Staf <lest0823@storsjogymnasiet.se>
2023-09-15 08:53:55 +0200

    fortnite9

--- About/about.js
+++ About/about.js
@@ -1,14 +1,4 @@
-let menuItem = document.getElementById("menu");
-let theDisplay = "none";
-
 function menuOpen() {
-    if (theDisplay == "none") {
-        console.log(menuItem.style.display);
-        theDisplay = "block";
-        menuItem.style.display='block';
-    }
-    else {
-        menuItem.style.display='none';
-        theDisplay = "none";
-    }
+    const menu = document.getElementById("menu");
+    menu.classList.toggle("visible");
 }
