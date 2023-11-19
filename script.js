6d9d70c078590c9571cc0e45a98c644ff270659c
Leo Staf <118734071+Le0Staf@users.noreply.github.com>
2023-11-19 11:40:31 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -14,3 +14,52 @@ repeatNinjaImage();
 function menuOpen() {
     document.getElementById("menu").classList.toggle("visible");
 } 
+
+let amongus1 = true;
+let amongus2 = true;
+let amongus3 = true;
+let amongus4 = true;
+let amongus5 = true;
+
+function Death(number) {
+    if (number == 1) {
+        if (amongus1 == true) {
+            var killsound = new Audio();
+            killsound.src = "kill.mp3";
+            killsound.play();
+            new Audio().src = "kill.mp3";
+            document.getElementById('amongus1').src='dead_body_standing_up.png';
+            amongus1 = false;
+        }
+    }
+    else if (number == 2) {
+        if (amongus2 == true) {
+            var killsound = new Audio();
+            killsound.src = "kill.mp3";
+            killsound.play();
+            new Audio().src = "kill.mp3";
+            document.getElementById('amongus2').src='dead_body_standing_up.png';
+            amongus2 = false;
+        }
+    }
+    else if (number == 3) {
+        if (amongus3 == true) {
+            var killsound = new Audio();
+            killsound.src = "kill.mp3";
+            killsound.play();
+            new Audio().src = "kill.mp3";
+            document.getElementById('amongus3').src='dead_body_standing_up.png';
+            amongus3 = false;
+        }
+    }
+    else if (number == 4) {
+        if (amongus4 == true) {
+            var killsound = new Audio();
+            killsound.src = "kill.mp3";
+            killsound.play();
+            new Audio().src = "kill.mp3";
+            document.getElementById('amongus4').src='dead_body_standing_up.png';
+            amongus4 = false;
+        }
+    }
+}
